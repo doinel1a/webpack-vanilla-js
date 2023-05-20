9e5398757b36fa5f033a8d075ac519e28db97f21
doinel1a <doinel1atanasiu@gmail.com>
2023-05-20 18:38:18 +0200

    refactor: favicon

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -70,11 +70,11 @@ module.exports = () => {
         },
         title: meta.title,
         icon192: isProduction
-          ? '/favicon/favicon-192.png'
-          : '/assets/favicon/favicon-192.png',
+          ? '/favicon/favicon-192.webp'
+          : '/assets/favicon/favicon-192.webp',
         icon512: isProduction
-          ? '/favicon/favicon-512.png'
-          : '/assets/favicon/favicon-512.png',
+          ? '/favicon/favicon-512.webp'
+          : '/assets/favicon/favicon-512.webp',
         manifest: isProduction ? '/app.webmanifest' : '/assets/app.webmanifest',
         sitemap: isProduction ? '/sitemap.xml' : '/assets/sitemap.xml',
         template: getDirectory('src/index.html'),
